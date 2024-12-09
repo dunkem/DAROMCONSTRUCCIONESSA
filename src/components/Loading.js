ec8d9c92384f8fe217a890d05645477a4a24dfa3
ADMINISTRACIÓN <dunkem55@gmail.com>
2024-12-09 08:56:45 -0300

    cambiosnecesarios9-12

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,10 @@
-// Loading.js
 import React from 'react';
 import './Loading.css'; // Asegúrate de crear este archivo CSS
 
 const Loading = () => {
     return (
         <div className="loading">
-            <img src="/logodaromtransparente.png" alt="Cargando..." />
-            
-            
+            <img src="/logodaromtransparente.png" alt="Logo de Darom SA cargando" />
         </div>
     );
 };
