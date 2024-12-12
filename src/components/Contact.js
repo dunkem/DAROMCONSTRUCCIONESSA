fd6bcf4b332765baf3f36af0a2d09339cb48b54d
ADMINISTRACIÓN <dunkem55@gmail.com>
2024-12-12 14:57:22 -0300

    cambioscontact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,16 +26,15 @@ function Contact() {
             event.stopPropagation();
         } else {
             const formData = new FormData(form);
-            formData.append('form-name', 'contact');
-
             fetch('/', {
                 method: 'POST',
-                body: formData,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams(formData).toString(), // Asegúrate de usar URLSearchParams
             })
             .then(() => {
                 setSubmitted(true);
                 setError('');
-                form.reset(); // Resetea el formulario después del envío
+                form.reset(); // Resetear el formulario después del envío
             })
             .catch(error => {
                 setError('Error al enviar el formulario');
@@ -54,7 +53,6 @@ function Contact() {
                       name="contact" 
                       method="POST" 
                       data-netlify="true" 
-                      data-netlify-honeypot="bot-field"
                       noValidate 
                       validated={validated} 
                       onSubmit={handleSubmit} 
@@ -101,9 +99,9 @@ function Contact() {
                         }
                     </p>
                     <p><i className="fas fa-map-marker-alt"></i> Oficina: {contactInfo.officeAddress}</p>
-                    <MapEmbed title="Ubicación Oficina" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13104.533593321816!2d-58.1568298!3d-34.8025856!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e64e395cdcd1%3A0xc9be6643f683c85!2sDarom%20Construcciones%20SRL!5e0!3m2!1ses!2sar!4v1732900240146!5m2!1ses!2sar" />
+                    <MapEmbed title="Ubicación Oficina" src="https://www.google.com/maps/embed?pb=..." />
                     <p className="mt-3"><i className="fas fa-industry"></i> Planta: {contactInfo.plantAddress}</p>
-                    <MapEmbed title="Ubicación Planta" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13097.319712146147!2d-58.1930728!3d-34.8479364!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2d7ff2e248f6b%3A0x13a6d078d9f675a2!2sPitec%20-%20Parque%20Industrial%20y%20Tecnol%C3%B3gico%20Florencio%20Varela!5e0!3m2!1ses!2sar!4v1732899985240!5m2!1ses!2sar" />
+                    <MapEmbed title="Ubicación Planta" src="https://www.google.com/maps/embed?pb=..." />
                 </Col>
             </Row>
         </Container>
